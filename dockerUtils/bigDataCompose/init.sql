f966ed970f805b2bce32b1dfdbd463d6969853b8
Daniel Pineda <dpinedaj@unal.edu.co>
2020-07-08 15:55:52 -0500

    _

---
+++ dockerUtils/bigDataCompose/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS hue;
+grant all on hue.* to 'hue'@'172.27.1.13' identified by 'secretpassword';
+grant all on hue.* to 'root'@'172.27.1.13' identified by 'secret';
+flush privileges;
